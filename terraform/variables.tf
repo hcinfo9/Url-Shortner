1e16ad59ae08c9828431c5eb4a894c9933278931
hcinfo9 <hcinformatica9438@gmail.com>
2024-11-02 00:14:28 -0300

    Update CI workflow to use master branch and docker compose update e create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "credentials_file_path" {
+  description = "Path to the GCP credentials JSON file"
+  type        = string
+}
+
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
